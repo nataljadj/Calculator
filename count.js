e3ad5c1b3b70ca90e1fd9f2c02e675ad31839ee8
unknown <ndiedyk@KOTROL.softservecom.com>
2015-05-21 15:54:19 +0300

    simple calculator with grunt and sass plugin

---
+++ count.js
@@ -0,0 +1,28 @@
+	function output(num){
+		var field = document.getElementById('field');
+		field.value += num;
+	}
+	function actionu(act){
+		var field = document.getElementById('field');
+		field.value += act;
+	}
+	function dot(){
+		var field = document.getElementById('field');
+		field.value += ".";
+	}
+	function Ce(){
+		var field = document.getElementById('field');
+		field.value = "";
+	}
+
+	function evalu () {
+		var fieldVal = document.getElementById('field').value;
+		alert(fieldVal);
+		if(document.getElementById('field').value == ''){
+			alert("sfawf");
+		}
+		else{
+			var output = eval(fieldVal);  
+			document.getElementById('field').value = output;
+		}
+	} 
